108658af743f372fc2d97efba5ef52956c0462ff
Nicolás Rodríguez Álvarez <nicolas.rodalv@educa.jcyl.es>
2026-01-17 19:07:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import rateLimit from 'express-rate-limit';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,11 +9,18 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+app.use(limiter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
